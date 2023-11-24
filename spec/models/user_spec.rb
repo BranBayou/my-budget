4fea5e75b8bfcfe0ebad633c828fed6abeb5ac1b
BranBayou <berhan.baye@gmail.com>
2023-11-24 10:27:06 +0300

    Add specs

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,17 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  subject { User.new(name: 'Tom') }
+
+  before { subject.save }
+
+  it 'is not valid without a title' do
+    subject.name = nil
+    expect(subject).to_not be_valid
+  end
+
+  it 'is not valid when name is not a string' do
+    subject.name = 23
+    expect(subject).to be_invalid
+  end
 end
